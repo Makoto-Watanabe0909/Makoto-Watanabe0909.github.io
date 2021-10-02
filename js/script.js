36c7d6a324baafc61a3ef77dc2f04f46cb9c0fe1
Makoto-Watanabe0909 <waco0909@gmail.com>
2021-10-02 21:33:16 +0900

    dragAndDrop実装

--- js/script.js
+++ js/script.js
@@ -1,81 +1,32 @@
-(function(){
-	alert("Extra Texture!!!");
+//参考 : https://qiita.com/yonedaco/items/ea234e95473d739c7447
 
-    //要素の取得
-    var elements = document.getElementsByClassName("drag-and-drop");
+alert("Extra Texture!!! ver.7");
 
-    //要素内のクリックされた位置を取得するグローバル（のような）変数
-    var x;
-    var y;
+var newspaper = document.getElementById("newspaper"); //新聞を拾ってくる
+newspaper.style.position = "absolute"; //位置基準の変更
 
-    //マウスが要素内で押されたとき、又はタッチされたとき発火
-    for(var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("mousedown", mdown, false);
-        elements[i].addEventListener("touchstart", mdown, false);
-    }
+newspaper.onmousedown = function(event){
+	document.addEventListener("mousemove",onMouseMove); //マウス動かした時に呼ばれる関数を有効に
+}
 
-    //マウスが押された際の関数
-    function mdown(e) {
+newspaper.ondragstart = function(event){
+  return false;
+}//とりあえず不要な関数（一応なにかに使うかもしれんから書いとく）
 
-        //クラス名に .drag を追加
-        this.classList.add("drag");
+var onMouseMove = function(event){
+  var x = event.clientX;
+  var y = event.clientY;
+  var width = newspaper.offsetWidth;
+  var height = newspaper.offsetHeight;
+  newspaper.style.top = (y-height/2) + "px";
+  //newspaper.style.left = (x-width/2) + "px";
+}
 
-        //タッチデイベントとマウスのイベントの差異を吸収
-        if(e.type === "mousedown") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        }
-
-        //要素内の相対座標を取得
-        x = event.pageX - this.offsetLeft;
-        y = event.pageY - this.offsetTop;
-
-        //ムーブイベントにコールバック
-        document.body.addEventListener("mousemove", mmove, false);
-        document.body.addEventListener("touchmove", mmove, false);
-    }
-
-    //マウスカーソルが動いたときに発火
-    function mmove(e) {
-
-        //ドラッグしている要素を取得
-        var drag = document.getElementsByClassName("drag")[0];
-
-        //同様にマウスとタッチの差異を吸収
-        if(e.type === "mousemove") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        }
-
-        //フリックしたときに画面を動かさないようにデフォルト動作を抑制
-        e.preventDefault();
-
-        //マウスが動いた場所に要素を動かす
-        drag.style.top = event.pageY - y + "px";
-        drag.style.left = event.pageX - x + "px";
-
-        //マウスボタンが離されたとき、またはカーソルが外れたとき発火
-        drag.addEventListener("mouseup", mup, false);
-        document.body.addEventListener("mouseleave", mup, false);
-        drag.addEventListener("touchend", mup, false);
-        document.body.addEventListener("touchleave", mup, false);
-
-    }
-
-    //マウスボタンが上がったら発火
-    function mup(e) {
-        var drag = document.getElementsByClassName("drag")[0];
-
-        //ムーブベントハンドラの消去
-        document.body.removeEventListener("mousemove", mmove, false);
-        drag.removeEventListener("mouseup", mup, false);
-        document.body.removeEventListener("touchmove", mmove, false);
-        drag.removeEventListener("touchend", mup, false);
-
-        //クラス名 .drag も消す
-        drag.classList.remove("drag");
-    }
-
-})()
+newspaper.onmouseup = function(event){
+  var x = event.clientX;
+  var y = event.clientY;
+  var width = newspaper.offsetWidth;
+  var height = newspaper.offsetHeight;
+	
+  document.removeEventListener("mousemove",onMouseMove);
+}
