53bc194bbe39017ad186b37a1f2905a4d2c97c9a
Makoto-Watanabe0909 <waco0909@gmail.com>
2021-10-03 00:25:24 +0900

    骨格完成

--- js/script.js
+++ js/script.js
@@ -1,32 +1,106 @@
 //参考 : https://qiita.com/yonedaco/items/ea234e95473d739c7447
 
-alert("Extra Texture!!! ver.7");
+alert("Extra Texture!!!");
 
 var newspaper = document.getElementById("newspaper"); //新聞を拾ってくる
+var forTest = document.getElementById("forTest"); //デバッグ用オブジェクト
 newspaper.style.position = "absolute"; //位置基準の変更
 
-newspaper.onmousedown = function(event){
-	document.addEventListener("mousemove",onMouseMove); //マウス動かした時に呼ばれる関数を有効に
-}
+var npX; //新聞の現在位置
+var npY;
+var mouseX1; //クリックしたときの座標
+var mouseY1;
 
-newspaper.ondragstart = function(event){
-  return false;
-}//とりあえず不要な関数（一応なにかに使うかもしれんから書いとく）
+npX = newspaper.offsetLeft;
+npY = newspaper.offsetTop;
 
-var onMouseMove = function(event){
-  var x = event.clientX;
-  var y = event.clientY;
-  var width = newspaper.offsetWidth;
-  var height = newspaper.offsetHeight;
-  newspaper.style.top = (y-height/2) + "px";
-  //newspaper.style.left = (x-width/2) + "px";
-}
+if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)) {
+  newspaper.addEventListener('touchstart', touchStartEvent, false);
+  newspaper.addEventListener('touchmove', touchMoveEvent, false);
+  newspaper.addEventListener('touchend', touchEndEvent, false);
+  document.addEventListener('touchmove', disableScroll, { passive: false });
+} else {} //PCと端末で違う方法でドラッグの動作を実装している。
 
-newspaper.onmouseup = function(event){
-  var x = event.clientX;
-  var y = event.clientY;
-  var width = newspaper.offsetWidth;
-  var height = newspaper.offsetHeight;
+newspaper.onmousedown = function (event) { //マウスを押した時
 	
-  document.removeEventListener("mousemove",onMouseMove);
+  if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)) {
+  } else {
+    mouseX1 = event.clientX;
+    mouseY1 = event.clientY;
+  } //その瞬間の座標を記録
+
+  document.addEventListener("mousemove", onMouseMove); //マウスを動かした時に呼ばれる関数を有効に
+}
+
+newspaper.onmouseup = function (event) { //マウスを離した時
+  npX = newspaper.offsetLeft;
+  npY = newspaper.offsetTop;
+  document.removeEventListener("mousemove", onMouseMove);
+}
+
+newspaper.onmouseout = function (event) { //マウスが新聞から外れた時（マウスがウィンドウの外に出た場合を想定）
+  npX = newspaper.offsetLeft;
+  npY = newspaper.offsetTop;
+  document.removeEventListener("mousemove", onMouseMove);
+}
+
+newspaper.ondragstart = function (event) { //ドラッグを始めた時
+  return false;
+} //何か知らんけどこれが無いと綺麗に動かない。
+
+var onMouseMove = function (event) {
+  var mouseX2;
+  var mouseY2;
+
+  mouseX2 = event.clientX;
+  mouseY2 = event.clientY;
+	
+  newspaper.style.top = (npY + mouseY2 - mouseY1) + "px";
+  newspaper.style.left = (npX + mouseX2 - mouseX1) + "px";
+}
+
+/*==========以下、端末用============================================================*/
+
+function touchStartEvent(event) {
+  mouseX1 = event.touches[0].pageX;
+  mouseY1 = event.touches[0].pageY;
+}
+
+function touchMoveEvent(event) {
+  var mouseX2;
+  var mouseY2;
+	
+  mouseX2 = event.touches[0].pageX;
+  mouseY2 = event.touches[0].pageY;
+	
+  newspaper.style.top = (npY + mouseY2 - mouseY1) + "px";
+  newspaper.style.left = (npX + mouseX2 - mouseX1) + "px";
+}
+
+function touchEndEvent(event) {
+  npX = newspaper.offsetLeft;
+  npY = newspaper.offsetTop;
+}
+
+function disableScroll(event) {
+	
+  var xxx = event.touches[0].pageX;
+  var yyy = event.touches[0].pageY;
+	
+  var pX = newspaper.offsetLeft;
+  var pY = newspaper.offsetTop;
+  var pW = newspaper.width;
+  var pH = newspaper.height;
+	
+  if (xxx > pX && xxx < pX + pW && yyy > pY && yyy < pY + pH) {
+	  forTest.style.backgroundColor = "#00ff00";
+  	if (event.touches.length == 1) {
+	  	forTest.style.backgroundColor = "#0000ff";
+  		event.preventDefault();
+  	}else{
+	  	forTest.style.backgroundColor = "#ffffff";
+	}//スクロールは禁止　でもピンチはOK（接している指の本数で判定）
+  }else{
+	  forTest.style.backgroundColor = "#ff0000";
+  }
 }
