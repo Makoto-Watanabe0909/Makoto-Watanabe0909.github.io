6de1f896af49bd73535336b4cb33735c5c4338df
Makoto-Watanabe0909 <waco0909@gmail.com>
2022-02-11 21:43:47 +0900

    店内に音楽を流せるように

--- js/script_shelf.js
+++ js/script_shelf.js
@@ -1,29 +1,17 @@
-//参考 : https://qiita.com/WitTarou/items/7d791e0d6c5e2bc36c4c
-
 console.log("スクリプト動いてます");
 
-document.addEventListener('DOMContentLoaded', function () {
-    const resizeFix = function () {
-        const checkHeight = window.innerWidth / 1920 * 1080;
-        const marginTop = window.innerHeight - checkHeight;
-        const checkWidth = window.innerHeight / 1080 * 1920;
-        const marginLeft = window.innerWidth - checkWidth;
+var trackChoice = 0;
+var songsNum = 3;
 
-        if (checkHeight > window.innerHeight) { //
-            document.getElementsByClassName('wrap')[0].style.transform = 'scale(' + window.innerHeight / 1080 + ')';
-            document.getElementsByClassName('wrap')[0].style.marginLeft = marginLeft / 2 + 'px';
-            document.getElementsByClassName('wrap')[0].style.marginTop = 0 + 'px';
-        } else {
-            document.getElementsByClassName('wrap')[0].style.transform = 'scale(' + window.innerWidth / 1920 + ')';
-            document.getElementsByClassName('wrap')[0].style.marginLeft = 0 + 'px';
-            document.getElementsByClassName('wrap')[0].style.marginTop = marginTop / 2 + 'px';
-        }
-    }
+document.getElementById("recordplayer").onclick = function() {
 
-    resizeFix();
+  var song = document.getElementById('songbar');
+  var songlist = new Array('sound/A.mp3', 'sound/B.mp3', 'sound/C.mp3');
 
-    window.addEventListener('resize', function () {
-		console.log("リサイズ");
-        resizeFix();
-    });
-});
+  trackChoice = trackChoice + 1;
+  if(trackChoice >= songsNum)trackChoice = 0;
+	
+  song.src = songlist[trackChoice];
+  song.load();
+  song.play();
+};
